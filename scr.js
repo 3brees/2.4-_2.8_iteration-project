20e540dabc871c5231438b9ac80d103ce6e3d582
3brees <3brees@tawacollege.school.nz>
2026-09-16 21:50:49 +1200

    Create scr.js

---
+++ scr.js
@@ -0,0 +1,19 @@
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+mybutton.style.opacity = "100";
+  } else {
+    mybutton.style.opacity = "0";
+
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+}
